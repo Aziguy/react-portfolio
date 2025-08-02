e4ea1905725c038b87c4685bc126e9e8fe1ee381
Aziguy <monsieur_hernandez@hotmail.com>
2025-08-02 14:31:43 +0200

    evo: optimize experience component using data object.

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,204 +1,79 @@
-import React from 'react'
+import React from 'react';
 import { PiSealCheckFill } from "react-icons/pi";
-
-import './experience.css'
+import './experience.css';
 
 export const Experience = () => {
+  const experienceData = [
+  {
+    category: "Software Testing",
+    skills: [
+      { name: "Pytest", level: "Intermediate" },
+      { name: "Selenium", level: "Experienced" },
+      { name: "Appium", level: "Intermediate" },
+      { name: "Cypress", level: "Experienced" },
+      { name: "PlayWright", level: "Intermediate" },
+      { name: "Postman", level: "Intermediate" },
+    ],
+  },
+  {
+    category: "ALM Tools",
+    skills: [
+      { name: "RedMine", level: "Experienced" },
+      { name: "Jira", level: "Intermediate" },
+      { name: "TestRail", level: "Intermediate" },
+      { name: "Squash", level: "Intermediate" },
+      { name: "XRay", level: "Experienced" },
+      { name: "Gherkin | Cucumber", level: "Experienced" },
+    ],
+  },
+  {
+    category: "Frontend Development",
+    skills: [
+      { name: "HTML CSS JS", level: "Experienced" },
+      { name: "CSS Framework", level: "Intermediate" },
+      { name: "ReactJs", level: "Intermediate" },
+      { name: "TypeScript", level: "Intermediate" },
+      { name: "Hugo | Markdown", level: "Experienced" },
+      { name: "Vite | Gulp", level: "Experienced" },
+    ],
+  },
+  {
+    category: "Backend Development",
+    skills: [
+      { name: "Node JS", level: "Intermediate" },
+      { name: "PHP", level: "Experienced" },
+      { name: "Laravel", level: "Intermediate" },
+      { name: "WordPress", level: "Experienced" },
+      { name: "Python", level: "Intermediate" },
+      { name: "Csharp", level: "Intermediate" },
+    ],
+  },
+];
+
+
   return (
     <section id="experiences">
       <h5>What Skills I Have</h5>
       <h2>My Experience</h2>
 
       <div className="container experience__container">
-        <div className="experience__testing">
-          <h3>Software Testing</h3>
-          <div className="experience__content">
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Pytest</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Selenium</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Appium</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Cypress</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>PlayWright</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Postman</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
+        {experienceData.map(({ category, skills }) => (
+          <div key={category} className={`experience__${category.toLowerCase().replace(/\s+/g, '')}`}>
+            <h3>{category}</h3>
+            <div className="experience__content">
+              {skills.map(({ name, level }) => (
+                <article key={name} className="experience__details">
+                  <PiSealCheckFill className="experience__details-icon" />
+                  <div>
+                    <h4>{name}</h4>
+                    <small className="text-light">{level}</small>
+                  </div>
+                </article>
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="experience__alm">
-          <h3>ALM Tools</h3>
-          <div className="experience__content">
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>RedMine</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Jira</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>TestRail</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Squash</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>XRay</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Gherkin  | Cucumber</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-          </div>
-        </div>
-        <div className="experience__frontend">
-          <h3>Frontend Development</h3>
-          <div className="experience__content">
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>HTML CSS JS</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>CSS Framework</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>ReactJs</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>TypeScript</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Hugo | Markdown</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Vite | Gulp</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-          </div>
-        </div>
-        <div className="experience__backend">
-          <h3>Backend Development</h3>
-          <div className="experience__content">
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Node JS</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>PHP</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Laravel</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>WordPress</h4>
-                <small className='text-light'>Experienced</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Python</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <PiSealCheckFill className='experience__details-icon'/>
-              <div>
-                <h4>Csharp</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
-  )
-}
+  );
+};
