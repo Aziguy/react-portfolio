99b7f66d29139942304d8fb99d1c48053406ad5c
Aziguy <monsieur_hernandez@hotmail.com>
2025-08-14 12:28:40 +0200

    evo: use french into header component.

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,8 +4,8 @@ import CV from '../../assets/KENGNI_Hippolyte_CV_FR_082025_dev.pdf'
 export const CTA = () => {
   return (
     <div className='cta'>
-        <a href={CV} download className='btn'>Download CV</a>
-        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        <a href={CV} download className='btn'>Télécharger CV</a>
+        <a href="#contact" className='btn btn-primary'>Discutons</a>
     </div>
   )
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,17 +8,17 @@ export const Header = () => {
   return (
     <header>
       <div className='container header__container'>
-        <h5>Hello I'm</h5>
+        <h5>Salut je suis</h5>
         <h1>Hippolyte K.</h1>
-        <h5 className='text-light'>software developer with a specialization in automation software testing</h5>
+        <h5 className='text-light'>Développeur de logiciels spécialisé en assurance qualité logiciel et tests E2E automatisés</h5>
         <CTA />
         <HeaderSocial />
 
         <div className="me">
-          <img src={me} alt="Hippolyte" />
+          <img src={me} alt="Hippolyte K." />
         </div>
 
-        <a href="#contact" className='scroll__down'>Scroll Down</a>
+        <a href="#contact" className='scroll__down'>Vers le bas</a>
       </div>
     </header>
   )
